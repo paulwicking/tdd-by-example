259b1a6340c6fc054b57bd93bf32c0cce4a1fcca
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2018-02-06 08:23:14 +0100

    GREEN Add Franc

--- money_example.py
+++ money_example.py
@@ -16,3 +16,22 @@ class Dollar(object):
 
     def equals(self, comparand):
         return self.__eq__(comparand)
+
+
+class Franc(object):
+    def __init__(self, amount):
+        self.__amount = amount
+
+    def __repr__(self):
+        return repr(self.__amount)
+
+    def __eq__(self, other):
+        return self.__amount == other
+
+    def times(self, multiplier):
+        amount = self.__amount * multiplier
+        return Franc(amount)
+
+    def equals(self, comparand):
+        return self.__eq__(comparand)
+
--- tests/test_money_example.py
+++ tests/test_money_example.py
@@ -5,13 +5,14 @@ from money_example import *
 class TestDollar(TestCase):
     def test_multiplication(self):
         five = Dollar(5)
-        product = five.times(2)
-
-        self.assertEqual(10, product)
-
-        product = five.times(3)
-        self.assertEqual(15, product)
+        self.assertEqual(Dollar(10), five.times(2))
+        self.assertEqual(Dollar(15), five.times(3))
 
     def test_equality(self):
         self.assertTrue(Dollar(5).equals(Dollar(5)))
         self.assertFalse(Dollar(5).equals(Dollar(6)))
+
+    def test_franc_multiplication(self):
+        five = Franc(5)
+        self.assertEqual(Franc(10), five.times(2))
+        self.assertEqual(Franc(15), five.times(3))
